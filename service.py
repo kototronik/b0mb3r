e83e99da5195ef40d61eebca7650313060aa2377
kototronik <83447310+kototronik@users.noreply.github.com>
2021-04-30 20:18:19 +0900

    Add files via upload

--- service.py
+++ service.py
@@ -1,49 +1,78 @@
-import inspect
-import os
-import sys
-from functools import lru_cache
-from typing import Dict, Union
+import random
+import re
+import string
+from abc import ABC, abstractmethod
+from typing import Callable
 
+import httpx
 from loguru import logger
+from random_user_agent.user_agent import UserAgent
 
 
-@logger.catch
-@lru_cache(maxsize=None)
-def prepare_services(directory: str = "services") -> Dict[Union[int, str], list]:
-    loaded_services = load_services(directory)
-    all_phone_codes: Dict[Union[int, str], list] = {}
-    special_services = []
+class Service(ABC):
+    user_agent_rotator = UserAgent()
+    headers = {
+        "User-Agent": user_agent_rotator.get_random_user_agent(),
+        "X-Requested-With": "XMLHttpRequest",
+    }
+    country_codes = {"7": "ru", "375": "by", "380": "ua"}
+    phone_codes = []
+    client = httpx.AsyncClient(headers=headers, verify=False)
 
-    for service in loaded_services:
-        if len(service.phone_codes) == 0:
-            special_services.append(service)
+    def __init__(self, phone: str, country_code: int):
+        self.country_code = str(country_code)
+        self.phone = phone[len(self.country_code) :]
+        self.formatted_phone = phone
 
-        for phone_code in service.phone_codes:
-            if phone_code not in all_phone_codes:
-                all_phone_codes[phone_code] = []
-            all_phone_codes[phone_code].append(service)
+        self.russian_name = "".join(
+            random.choice(
+                "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+            )
+            for _ in range(5)
+        )
+        self.username = self.password = "".join(
+            random.choice(string.ascii_letters) for _ in range(12)
+        )
+        self.email = (
+            f"{self.username}@{random.choice(['gmail.com', 'mail.ru', 'yandex.ru'])}"
+        )
 
-    for special_service in special_services:
-        for key in all_phone_codes:
-            all_phone_codes[key].append(special_service)
+    async def get(self, *args, **kwargs):
+        return await self.request_logger(self.client.get, *args, **kwargs)
 
-    all_phone_codes["other"] = special_services
+    async def post(self, *args, **kwargs):
+        return await self.request_logger(self.client.post, *args, **kwargs)
 
-    logger.success("Services preparation complete")
-    return all_phone_codes
+    async def options(self, *args, **kwargs):
+        return await self.request_logger(self.client.options, *args, **kwargs)
 
+    async def request_logger(self, function: Callable, *args, **kwargs):
+        response = await function(*args, **kwargs, timeout=3)
+        if response.is_error:
+            logger.info(
+                f"{self.__class__.__name__} returned an error HTTP code: {response.status_code}"
+            )
 
-@logger.catch
-def load_services(directory: str) -> list:
-    files = os.listdir(directory)
-    sys.path.insert(0, directory)
-    loaded_services = []
+        return response
 
-    for file in files:
-        if file.endswith(".py") and file != "service.py":
-            module = __import__(file.replace(".py", ""))
-            for member in inspect.getmembers(module, inspect.isclass):
-                if member[1].__module__ == module.__name__:
-                    loaded_services.append(getattr(module, member[0]))
+    async def get_csrf_token(self, url: str, pattern):
+        response = await self.get(url)
+        return re.search(pattern, response.text).group(1).strip()
 
-    return loaded_services
+    @staticmethod
+    def format(phone: str, mask: str, mask_symbol: str = "*"):
+        if len(phone) == mask.count(mask_symbol):
+            formatted_phone = ""
+            for symbol in mask:
+                if symbol == mask_symbol:
+                    formatted_phone += phone[0]
+                    phone = phone[(len(phone) - 1) * -1 :]
+                else:
+                    formatted_phone += symbol
+        else:
+            formatted_phone = phone
+        return formatted_phone
+
+    @abstractmethod
+    async def run(self):
+        raise NotImplementedError
